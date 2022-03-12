0ec9b2845727eee027695b372a7bcf159d9d9759
Just_Melo <justmework@gmail.com>
2022-03-12 03:16:12 +0300

    Добавил описание к фотографиям

--- js/draw-photo.js
+++ js/draw-photo.js
@@ -7,10 +7,11 @@ const UsersPhotoList = document.createDocumentFragment();
 const createPhotoPosts = (PhotoCardsData) => {
   for (let i = 0; i < PhotoCardsData.length; i++) {
     const newPhoto = photoPostTemplate.cloneNode(true);
-    const photoUrl = newPhoto.querySelector('.picture__img');
+    const photoImg = newPhoto.querySelector('.picture__img');
     const photoComments = newPhoto.querySelector('.picture__comments');
     const photoLikes = newPhoto.querySelector('.picture__likes');
-    photoUrl.src = PhotoCardsData[i].url;
+    photoImg.src = PhotoCardsData[i].url;
+    photoImg.alt = PhotoCardsData[i].description;
     photoComments.textContent = PhotoCardsData[i].comments.length;
     photoLikes.textContent = PhotoCardsData[i].likes;
     UsersPhotoList.append(newPhoto);
