eb6ffdb894fc258529b489f99c1f6346f337f954
Just_Melo <justmework@gmail.com>
2022-03-12 03:22:48 +0300

    поправил название переменной

--- js/draw-photo.js
+++ js/draw-photo.js
@@ -2,7 +2,7 @@ import { createPhotoCardsData, REQUIRED_PHOTOCARDS } from './data.js';
 
 const photoContainer = document.querySelector('.pictures');
 const photoPostTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const UsersPhotoList = document.createDocumentFragment();
+const usersPhotoList = document.createDocumentFragment();
 
 const createPhotoPosts = (PhotoCardsData) => {
   for (let i = 0; i < PhotoCardsData.length; i++) {
@@ -14,13 +14,13 @@ const createPhotoPosts = (PhotoCardsData) => {
     photoImg.alt = PhotoCardsData[i].description;
     photoComments.textContent = PhotoCardsData[i].comments.length;
     photoLikes.textContent = PhotoCardsData[i].likes;
-    UsersPhotoList.append(newPhoto);
+    usersPhotoList.append(newPhoto);
   }
 };
 
 const drawUsersPhotos = () => {
   createPhotoPosts(createPhotoCardsData(REQUIRED_PHOTOCARDS));
-  photoContainer.append(UsersPhotoList);
+  photoContainer.append(usersPhotoList);
 };
 
 drawUsersPhotos();
